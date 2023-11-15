9f31d61d7efc5fa86c02986b0f45c5647e81809b
B4tiste <batiste.laloi@gmail.com>
2023-11-15 16:15:39 +0100

    Test onclick

--- js/Akinator.js
+++ js/Akinator.js
@@ -18,32 +18,37 @@ $(document).ready(function () {
 
 	$('#page_start').on('click', function() {
         console.log("click start");
+
         raise('AkinatorDialog/answer', "start")
     });
 
     $('#page_selection_oui').on('click', function() {
         console.log("click oui");
-        raise('AkinatorDialog/oui', "oui")
+        raise('AkinatorDialog/answer', "oui")
     });
 
     $('#page_selection_probablement').on('click', function() {
         console.log("click probablement");
-        raise('AkinatorDialog/probablement', "probablement")
+        raise('AkinatorDialog/answer', "probablement")
     });
 
     $('#page_selection_idk').on('click', function() {
         console.log("click je ne sais pas");
-        raise('AkinatorDialog/idk', "idk")
+        raise('AkinatorDialog/answer', "idk")
     });
 
     $('#page_selection_probablement_pas').on('click', function() {
         console.log("click probablement pas");
-        raise('AkinatorDialog/probablement_pas', "probablement_pas")
+        raise('AkinatorDialog/answer', "probablement_pas")
     });
 
     $('#page_selection_non').on('click', function() {
         console.log("click Non");
-        raise('AkinatorDialog/Non', "non")
+        raise('AkinatorDialog/answer', "non")
     });
 
 });
+
+function myFunction() {
+    document.getElementById("test").innerHTML = "YOU CLICKED ME!";
+}
