cc824cba00fe2f6807b40a498809dd985c8b0b4a
Keelah Atom <al.loncham@gmail.com>
2023-11-27 14:19:44 +0100

    Revert "Added Metrics"
    This reverts commit b2983ee5d2390a9632878de13760eede46a3640d.

--- Apinator/Apinator/Program.cs
+++ Apinator/Apinator/Program.cs
@@ -18,25 +18,7 @@ builder.Services.AddSingleton<AkiDb>();
 
 builder.Services.AddAkinator();
 
-// builder.Services.AddOpenTelemetry()
-//     .WithMetrics(builder =>
-//     {
-//         // builder.AddPrometheusExporter();
-
-//         builder.AddMeter("Microsoft.AspNetCore.Hosting",
-//                          "Microsoft.AspNetCore.Server.Kestrel");
-//         // builder.AddView("http.server.request.duration");
-//         // builder.AddView("http.server.request.duration",
-//         //     new ExplicitBucketHistogramConfiguration
-//         //     {
-//         //         Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
-//         //                0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
-//         //     });
-//     });
-
 var app = builder.Build();
-
-// app.MapPrometheusScrapingEndpoint();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -47,24 +29,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// app.Use(async (context, next) =>
-// {
-//     var tagsFeature = context.Features.Get<IHttpMetricsTagsFeature>();
-//     if (tagsFeature != null)
-//     {
-//         var source = context.Request.Query["utm_medium"].ToString() switch
-//         {
-//             "" => "none",
-//             "social" => "social",
-//             "email" => "email",
-//             "organic" => "organic",
-//             _ => "other"
-//         };
-//         tagsFeature.Tags.Add(new KeyValuePair<string, object?>("mkt_medium", source));
-//     }
-
-//     await next.Invoke();
-// });
 
 app.MapGet("/ping", Ping)
 .WithName("Ping")
